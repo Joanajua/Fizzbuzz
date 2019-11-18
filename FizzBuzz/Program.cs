6fa48498f940bf5937d3f1bee2a1ea147b8f1310
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2019-11-18 12:40:24 +0000

    Part 3

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,7 +13,14 @@ namespace FizzBuzz
         {
             for(int i=1; i<=100; i++)
             {
-                Console.WriteLine(i);
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else 
+               {
+                    Console.WriteLine(i);
+                }
             }
             Console.ReadKey();
         }
