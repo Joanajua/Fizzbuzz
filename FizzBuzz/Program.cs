88a4d247b9b6d260572b9d31f807196d72300091
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2019-11-18 14:24:33 +0000

    StrechGoals 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,16 +13,31 @@ namespace FizzBuzz
         {
             for(int i=1; i<=100; i++)
             {
-                
-                if (i % 3 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 && i % 7 == 0 )
+                {
+                    Console.WriteLine("FizzBang");
+                }
+                else if (i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("BangBuzz");
+                }
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
-                else if (i % 5 == 0)
+                else if  (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
-              
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Bang");
+                }
+
                 else
                 {
                     Console.WriteLine(i);
