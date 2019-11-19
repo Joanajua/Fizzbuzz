326be1ec5f2ee4b9111f08bbc458b3a99613b648
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2019-11-19 09:56:58 +0000

    Part 3

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,76 +8,74 @@ namespace FizzBuzz
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            for(int i=1; i<=200; i++)
+            String Fizz = "Fizz"; //3
+            String Buzz = "Buzz"; //5
+            String Bang = "Bang"; //7
+            String Bong = "Bong"; //11
+            String Fezz = "Fezz"; //13
+
+            for (int i=1; i<=300; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
+
+                if (i % 11 == 0)
                 {
-                    if (i % 11 == 0)
-                    {
-                        Console.WriteLine("Bong");
-                    }
+                    Console.WriteLine(Bong);
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
                     if (i % 13 == 0)
                     {
-                        Console.WriteLine("FizzFezzBuzz");
+                        string outcome = Fizz + Fezz + Buzz;
+                        Console.WriteLine(outcome);
                     }
-                    else
-                    {
-                        Console.WriteLine("FizzBuzz");
+                    else                    {
+                        Console.WriteLine(Fizz + Buzz);
                     }
                 }
                 else if (i % 3 == 0 && i % 7 == 0 )
                 {
-                    if (i % 11 == 0)
-                    {
-                        Console.WriteLine("Bong");
-                    }
                     if (i % 13 == 0)
                     {
-                        Console.WriteLine("FizzFezzBang");
+                        string outcome = Fizz + Fezz + Bang;
+                        Console.WriteLine(outcome);
                     }
                     else
                     {
-                        Console.WriteLine("FizzBang");
+                        Console.WriteLine(Fizz + Bang);
                     }
                 }
                 else if (i % 5 == 0 && i % 7 == 0)
                 {
-                    if (i % 11 == 0)
-                    {
-                        Console.WriteLine("Bong");
-                    }
                     if (i % 13 == 0)
                     {
-                        Console.WriteLine("FezzBangBuzz");
+                        string outcome = Fezz + Bang + Buzz;
+                        Console.WriteLine(outcome);
                     }
                     else
                     {
-                        Console.WriteLine("BangBuzz");
+                        Console.WriteLine(Bang + Buzz);
 
                     }
                 }
                 else if (i % 13 == 0)
                 {
-                    Console.WriteLine("Fezz");
+                    Console.WriteLine(Fezz);
                 }
-                else if (i % 11 == 0)
-                {
-                    Console.WriteLine("Bong");
-                }
+                
                 else if (i % 3 == 0)
                 {
-                    Console.WriteLine("Fizz");
+                    Console.WriteLine(Fizz);
                 }
                 else if  (i % 5 == 0)
                 {
-                    Console.WriteLine("Buzz");
+                    Console.WriteLine(Buzz);
                 }
                 else if (i % 7 == 0)
                 {
-                    Console.WriteLine("Bang");
+                    Console.WriteLine(Bang);
                 }
               
 
